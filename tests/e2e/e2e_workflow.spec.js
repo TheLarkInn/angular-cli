d4dc90b56827b56ff1486c57ccf2ca86eb90bd39
Sean Larkin <sean.larkin@cuw.edu>
2016-07-17 18:52:44 -0500

    fix(): Multiple fixes for path mappings:
    * Removed invalid test case that was throwing path mappings  tests because of a typescript bug.
    * Readded PathsPlugin for Path Mappings
    * Removed coverage preprocessor which was throwing bad type errors against ts code when it shouldn't be.
    * Added sourcemap support for istanbul instrumenter. Coverage files are still being generated.

--- tests/e2e/e2e_workflow.spec.js
+++ tests/e2e/e2e_workflow.spec.js
@@ -458,6 +458,7 @@ describe('Basic end-to-end Workflow', function () {
     config.compilerOptions.paths = { '@angular/*': [] };
     fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), 'utf8');
 
+<<<<<<< HEAD
     sh.exec(`${ngBin} build`);
     // #TODO: Uncomment these lines when https://github.com/Microsoft/TypeScript/issues/9772 is fixed.
     // .catch(() => {
@@ -477,6 +478,27 @@ describe('Basic end-to-end Workflow', function () {
     expect(existsSync(path.join(process.cwd(), 'dist'))).to.be.equal(true);
     const indexHtml = fs.readFileSync(path.join(process.cwd(), 'dist/index.html'), 'utf-8');
     expect(indexHtml).to.include('main.bundle.js');
+=======
+    return exec(`${ngBin} build`)
+      // #TODO: Uncomment these lines when https://github.com/Microsoft/TypeScript/issues/9772 is fixed.
+      // .catch(() => {
+      //   return true;
+      // })
+      // .then((passed) => {
+      //   expect(passed).to.equal(true);
+      // })
+      .then(() => {
+        // This should succeed.
+        config.compilerOptions.paths = {
+          '@angular/*': [ '../node_modules/@angular/*' ]
+        };
+        fs.writeFileSync(configFilePath, JSON.stringify(config, null, 2), 'utf8');
+      })
+      .then(() => exec(`${ngBin} build`)
+      .catch(() => {
+        expect('build failed where it should have succeeded').to.equal('');
+      });
+>>>>>>> e3e637b... fix: fix the way the build command runs for mobile tests
   });
 
   it('Serve and run e2e tests after all other commands', function () {
